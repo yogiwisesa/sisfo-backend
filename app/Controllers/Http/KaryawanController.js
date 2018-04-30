9a858b6cd77cbe8e5d940021e1ad5552958701e0
Yogi Wisesa <yogiwisesa@gmail.com>
2018-04-30 21:18:52 +0800

    init commit

---
+++ app/Controllers/Http/KaryawanController.js
@@ -0,0 +1,48 @@
+'use strict'
+const Database = use('Database')
+
+class KaryawanController {
+
+    async postLogin({request}){
+        let {username, password} = request.all()
+        let account = await Database.select('*').from('karyawan').where({'username': username, 'password': password})
+        if (account.length > 0){
+            return {
+                message: 'ok',
+                account
+            }
+        } else {
+            return {
+                message: 'User not found'
+            }
+        }
+    }
+
+    async postJadwal({request}) {
+        let {id} = request.only(['id'])
+        let data = await Database.select('*').from('TableJadwal').where('id_karyawan', id)
+        if (data.length > 0){
+            return {
+                message: 'ok',
+                data
+            }
+        } else {
+            return {
+                message: `user's id not found`
+            }
+        }  
+    }
+
+    async postRequestTukar({request}){
+         let {idjadwal1, idjadwal2} = request.only(['idjadwal1', 'idjadwal2'])
+         let data = await Database.table('TabelTukar').insert({ 
+             id_jadwal1: idjadwal1,
+             id_jadwal2: idjadwal2,
+             status: 'Belum dikonfirmasi'
+         })
+
+         return data
+    }
+}
+
+module.exports = KaryawanController
